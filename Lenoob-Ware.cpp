dd31b7c0bf44ab4dbfbe4a3b9342a94423d1d817
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2022-08-28 16:37:52 -0700

    efew

--- Lenoob-Ware.cpp
+++ Lenoob-Ware.cpp
@@ -5,8 +5,10 @@
 #include <vector>
 
 #include "DeezNutsInUrMouth.h"
-int main()
-{    std::vector< int* > v;
+
+void e()
+{
+    std::vector< int* > v;
     std::cout << "Darius what you doing in my closet!\n";
     while(true)
     {
@@ -17,5 +19,53 @@ int main()
         
     }
 }
-
+void showmainscreen()
+{
+    std::cout.clear();
+    LogUtils::Log("[1] funny pc joe edition halal mode || [2] HWID Spoofer");
+    LogUtils::Log("Enter arg:");
+    char s;
+    std::cin >> s;
+    
+    switch (s)
+    {
+    case '1':
+            
+        break;
+    case '2':
+            
+        break;
+    default:
+        LogUtils::Log("Please Enter a valid input");
+        showmainscreen();
+        break;
+    }
+}
+void SpoofF()
+{
+   
+}
+int main()
+{
+    std::vector< int* > v;
+    LogUtils::Log("[1] funny pc joe edition halal mode || [2] HWID Spoofer");
+    LogUtils::Log("Enter arg:");
+    char s;
+    std::cin >> s;
+    
+    switch (s)
+    {
+        case '1':
+            e();
+            break;
+        case '2':
+            
+            break;
+    default:
+        LogUtils::Log("Please Enter a valid input");
+        showmainscreen();
+        break;
+    }
+    
+}
 
